68e4dde329e63c3735585ba1be4b4d5cceaa879b
Shiny2404 <shinypriya2402@gmail.com>
2026-04-25 23:48:45 +0530

    Add files via upload

---
+++ main4.py
@@ -0,0 +1,10 @@
+import qrcode
+from PIL import Image
+
+qr=qrcode.QRCode(version=1,
+                 error_correction=qrcode.constants.ERROR_CORRECT_H,
+                 box_size=10,border=4,)
+qr.add_data("http://github.com/Shiny2404")
+qr.make(fit=True)
+img=qr.make_image(fill_color="green",back_color="white")
+img.save("shiny_Github.png")
